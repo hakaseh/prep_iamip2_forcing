66cc448a3a8a4b0cc553fe45f92bce2f5637b25c
hakase hayashida <hakase.hayashida@utas.edu.au>
2021-07-22 11:13:06 +1000

    fix link.sh. works for now, but ideally i should create arrays for version and grid type (gr/gn) like done for vlist and flist.

--- link.sh
+++ link.sh
@@ -6,7 +6,7 @@
 insti=EC-Earth-Consortium #CMCC #MRI
 model=EC-Earth3 #CMCC-ESM2 #MRI-ESM2-0
 mip=ScenarioMIP
-exp=ssp126
+exp=ssp585 #126
 variant=r1i1p1f1
 version=v20200310 #v20210126 #v20191108
 moddir=/g/data/oi10/replicas/CMIP6/${mip}/${insti}/${model}/${exp}/${variant}/
@@ -14,9 +14,10 @@ moddir=/g/data/oi10/replicas/CMIP6/${mip}/${insti}/${model}/${exp}/${variant}/
 #For the case of CMCC-ESM2, the only unavailable variable is prsn and psl, which were downloaded in the next step (wget.sh)
 #CMCC-ESM2 ssp126: missing prsn, psl, friver
 #CMCC-ESM2 ssp585: missing prsn, psl
+#EC-Earth3 ssp585: missing prsn, psl
 #EC-Earth3 ssp126: missing prsn, psl, friver
-vlist=(huss tas uas vas rsds rlds pr) #friver)
-flist=(3hr  3hr 3hr 3hr 3hr  3hr  3hr) #Omon)
+vlist=(huss tas uas vas rsds rlds pr friver)
+flist=(3hr  3hr 3hr 3hr 3hr  3hr  3hr Omon)
 
 #User input ends
 
@@ -28,12 +29,16 @@ do
     if [ $model = 'MRI-ESM2-0' ] && [ ${vlist[it2]} = 'friver' ]
     then
         ln -s ${moddir}/${flist[it2]}/${vlist[it2]}/gn/v20210329/${vlist[it2]}_${flist[it2]}_${model}_${exp}_${variant}_gn_*.nc $outdir
+    #EC-Earth3 is given on regular grid (gr), except for friver (gn).
+    elif [ $model = 'EC-Earth3' ]
+    then
+        if [ ${vlist[it2]} = 'friver' ]
+        then
+            ln -s ${moddir}/${flist[it2]}/${vlist[it2]}/gn/v20200918/${vlist[it2]}_${flist[it2]}_${model}_${exp}_${variant}_gn_*.nc $outdir
+        else
+            ln -s ${moddir}/${flist[it2]}/${vlist[it2]}/gr/${version}/${vlist[it2]}_${flist[it2]}_${model}_${exp}_${variant}_gr_*.nc $outdir
+        fi
     else
         ln -s ${moddir}/${flist[it2]}/${vlist[it2]}/gn/${version}/${vlist[it2]}_${flist[it2]}_${model}_${exp}_${variant}_gn_*.nc $outdir
-    fi
-    #EC-Earth3 is given on regular grid (gr).
-    if [ $model = 'EC-Earth3' ]
-    then
-        ln -s ${moddir}/${flist[it2]}/${vlist[it2]}/gr/${version}/${vlist[it2]}_${flist[it2]}_${model}_${exp}_${variant}_gr_*.nc $outdir
     fi
 done
